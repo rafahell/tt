f8cf973be43a9eae68e940d7408dbb789ea2df15
Rafael <220rrs@gmail.com>
2015-09-22 19:14:52 +0100

    end-day22

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,91 +1,189 @@
 /* jshint devel:true */
 
+var l = function(message) {
+    console.log(message);
+}
+
 //console.log('\'Allo \'Allo!');
 
-
-function initialize() {		
-	var locations = [
-	  ['Dublin Castle', 53.3428, -6.26742,'images/pin.svg'],
-	  ['Trinity College', 53.344016, -6.254461, 'images/pin.svg'],
-	  ['Dame Street', 53.344798, -6.264685, 'images/pin.svg'],
-	  ['Guinness Storehouse', 53.341648, -6.286024, 'images/pin.svg'],
-	  ['St. Patricks Church', 53.342878, -6.270620, 'images/pin.svg']
-	];
-	var map;
-	var markers = [];
+       var locations = [
+    [
+        "<div class='button-close'></div><div class='image'><img src='http://placehold.it/320x200'/></div><br/><p>Dublin Castle</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p><a href='http://www.deanhoteldublin.ie'>www.deanhoteldublin.ie</a><div class='buttons-container'><img class='prev-button' src='images/prev.svg' width='35' height='35'><img class='next-button' src='images/next.svg' width='35' height='35'></div>",
+    53.343021 , -6.267411,
+    1],
+    [
+        "<div class='button-close'></div><div class='image'><img src='http://placehold.it/320x200'/></div><br/><p>Trinity College</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p><a href='http://www.deanhoteldublin.ie'>www.deanhoteldublin.ie</a><div class='buttons-container'><img class='prev-button' src='images/prev.svg' width='35' height='35'><img class='next-button' src='images/next.svg' width='35' height='35'></div>",
+    53.343805, -6.254550,
+    2],
+    [
+        "<div class='button-close'></div><div class='image'><img src='http://placehold.it/320x200'/></div><br/><p>St Patricks Church</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p><a href='http://www.deanhoteldublin.ie'>www.deanhoteldublin.ie</a><div class='buttons-container'><img class='prev-button' src='images/prev.svg' width='35' height='35'><img class='next-button' src='images/next.svg' width='35' height='35'></div>",
+    53.340241, -6.271209 ,
+    3],
     
-    
-	map = new google.maps.Map(document.getElementById('map_canvas'), {
-	zoom: 14,
-	center: new google.maps.LatLng(53.3428,-6.26742),
-	mapTypeId: google.maps.MapTypeId.ROADMAP,
-	scrollwheel: false,
-	draggable: true
-	});
+];
+var markers = [];
+var map;
 
-
-	var num_markers = locations.length;
-  	for (var i = 0; i < num_markers; i++) {  
-	    markers[i] = new google.maps.Marker({
-	      position: {lat:locations[i][1], lng:locations[i][2]},
-	      map: map,
-	      html: locations[i][0],
-	      id: i,
-	      icon: locations[i][3]
-	    });
-    } //for
-
-    var infowindow = null;
-	/* now inside your initialise function */
-	infowindow = new google.maps.InfoWindow({
-	content: "holding..."
-	});
-
-	for (var i = 0; i < markers.length; i++) {
-	var marker = markers[i];	
-	google.maps.event.addListener(marker, 'click', function (e) {
-		
-
-	//infowindow.setContent(this.html);
-	//infowindow.open(map, this);
-	 
-	document.getElementById('info-container').innerHTML = this.html;
-
+function initialize() {
         
-	if ($("#info-container").css("display") === "block") {
-            $("#info-container").css("display", "none");
-            this.setIcon("images/pin.svg");
-        } else {
-            $("#info-container").animate({width:'toggle'},350);
-           	this.setIcon("images/pin-hover.svg");
-        }
-	});
+    map = new google.maps.Map(document.getElementById('map_canvas'), {
+        minZoom: 10, 
+	    maxZoom: 20, 
+        zoom: 14,
+        center: new google.maps.LatLng(53.347584, -6.259391),
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        zoomControl: true,
+        scrollwheel: false,
+        draggable: false,
+        
+    });
 
-	}
+    map.set('styles', [
+      {
+        featureType: 'road',
+        elementType: 'geometry',
+        stylers: [
+          { color: '#FFFFFF' }
+        ]
+      }, {
+        featureType: 'road',
+        elementType: 'labels',
+        stylers: [
+          { "visibility": "off" }
+        ]
+      },{
+        "featureType": "transit",
+        "elementType": "labels.icon",
+        "stylers": [
+          { "visibility": "off" }
+        ]
+      },
+      {
+        "featureType": "transit",
+        "elementType": "geometry",
+        "stylers": [
+          { "visibility": "off" }
+        ]
+      },
+       {
+        "featureType": "administrative",
+        "elementType": "labels",
+        "stylers": [
+          { "visibility": "off" }
+        ]
+      },
+      {
+        "featureType": "administrative.neighborhood",
+        "elementType": "labels",
+        "stylers": [
+          { "visibility": "off" }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "labels",
+        "stylers": [
+          { "visibility": "off" }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "labels.text",
+        "stylers": [
+          { "visibility": "off" }
+        ]
+      },        
+      {
+        featureType: 'landscape',
+        elementType: 'geometry',
+        stylers: [
+          { "visibility": "off" }
+        ]
+      }, {
+        featureType: 'poi.school',
+        elementType: 'geometry',
+        stylers: [
+         { "visibility": "off" }
+        ]
+      },{
+        "featureType": "poi.park",
+        "elementType": "geometry.fill",
+        "stylers": [
+            { "color": "#02a182" },
+            { "visibility": "on" }
+        ]
+      },
+        {
+        "featureType": "poi.park",
+        "elementType": "labels",
+        "stylers": [
+          { "visibility": "off" }
+        ]
+      }
+    ]);
+    
+    var infowindow = new google.maps.InfoWindow();
+    var iconBase = 'images/pin.svg';
+    var marker, i;
 
-	
-} //initialize() 
+    for (i = 0; i < locations.length; i++) {
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+            map: map,
+            icon: iconBase
+        });
+
+        google.maps.event.addListener(marker, 'click', (function (marker, i) {
+            
+            
+            return function (e) {
+                infowindow.setContent(locations[i][0], locations[i][6]);
+                infowindow.close(map, marker);
+                for (var j = 0; j < markers.length; j++) {
+                    markers[j].setIcon("images/pin.svg");
+                }
+                document.getElementById('info-container').innerHTML = locations[i][0];                     marker.setIcon("images/pin-hover.svg");
+
+                //pan map to exact location of the pin      
+                var newH = marker.getPosition().H - 0.0000;
+                var newL = marker.getPosition().L - -0.0200;         
+                map.panTo( new google.maps.LatLng( newH, newL ) );
+
+                $('.button-close').on("click",function(){
+                  $("#info-container").fadeOut();
+                });
+
+                $("#info-container").fadeIn("normal");
+
+            };
+        })(marker, i));
+        
+        markers.push(marker);
+    }
+    
+    $(document).on("click", ".prev-button", function(){
+            l('work');
+        
+              
+    });
+}
 
 google.maps.event.addDomListener(window, 'load', initialize);
 
-
-
 $(document).foundation({
      accordion: {
-    // specify the class used for accordion panels
-    content_class: 'content',
-    // specify the class used for active (or open) accordion panels
-    active_class: 'active',
-    // allow multiple accordion panels to be active at the same time
-    multi_expand: false,
-    // allow accordion panels to be closed by clicking on their headers
-    // setting to false only closes accordion panels when another is opened
-    toggleable: true
-  }
+        content_class: 'content',
+        active_class: 'active',
+        multi_expand: false,
+        toggleable: true
+      }
 });
 
+
+//Intro Text
 $(".centered").fadeIn('slow').delay(3450);
 
+//Slides 
 $( document ).ready(function() {
     //slides
 	$('#slides').slidesjs({
@@ -100,8 +198,6 @@ $( document ).ready(function() {
         navigation: false,
         play: {
           active: false,
-            // [boolean] Generate the play and stop buttons.
-            // You cannot use your own buttons. Sorry.
           effect: "slide",
             // [string] Can be either "slide" or "fade".
           interval: 9000,
@@ -118,10 +214,13 @@ $( document ).ready(function() {
 	});
 });
 
-    /*responsive nav menu*/
+    //responsive nav menu
 	$(".menu-opener").click(function(){
-  		$(".menu-opener, .menu-opener-inner, .menu").toggleClass("active");
+        $(".menu-opener, .menu-opener-inner, .menu").toggleClass("active");
 	});
+    $('.menu-inner a li').click(function() {
+      $(".menu-opener, .menu-opener-inner, .menu").removeClass("active");
+    });
 
 
     // when you click a menu item
@@ -151,40 +250,29 @@ $( document ).ready(function() {
 		event.preventDefault();
 		$('html, body').animate({scrollTop: -65}, 900);
 	});
+
     
-    /*$(".accordion li").on("click", "a:eq(0)", function (event) {
-    var li_parent = $(this).parent();
-
-        if (li_parent.hasClass('active')) {
-            $(".accordion li div.content:visible").slideUp("normal");
-        } else {
-            $(".accordion li div.content:visible").slideUp("normal");
-            $(this).parent().find(".content").slideToggle("normal");
-        }
-    });*/
-
-
     $(window).on('scroll', function(e) {
+        if($(window).width() > 1024) {
+        console.log('scrolling');
        
-       console.log('scrolling');
-       
-	   var navHeight = $( window ).height() - 0;
-       var height = $('#bgIntro').height();    
+           var navHeight = $(window).height() - 0;
+           var height = $('#bgIntro').height();    
 			 if ($(window).scrollTop() > navHeight) {
-				 $('nav').addClass('fixed');
+				 //$('nav').addClass('fixed');
                  //$('#bgIntro').remove();
-                 $(document).scrollTop($(document).scrollTop() - height);
                  $('#bgIntro').remove();
+                 $(document).scrollTop($(window).scrollTop() - height);   
 			 }
 			 else {
 				 $('nav').removeClass('fixed');
 			 }
-            if ($('#bgIntro').length == 0){
-                $('nav').addClass('fixed');
-                $('nav').css('margin-top', '0');
-            }
+             if ($('#bgIntro').length == 0){
+                 $('nav').addClass('fixed');
+                 $('nav').css('margin-top', '0');
+             }
+        }
         event.preventDefault();
-        
     });
     
     // console log wrapper so you only have to type l() not console.log()
@@ -214,5 +302,3 @@ $( document ).ready(function() {
             }
         });   
     });
-
-
